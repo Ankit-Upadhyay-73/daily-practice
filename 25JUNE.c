20bb8fda2fa76422f3cb6bc2da99ca842d1deae9
Ankit-Upadhyay-73 <55012101+Ankit-Upadhyay-73@users.noreply.github.com>
2022-06-25 23:04:12 +0530

    user console input

---
+++ 25JUNE.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+    char raw[20];
+
+    puts("enter whaterver you want, break");
+    scanf("%s",raw);
+
+    printf("Anything you typed is shown with \nHere\nHere..\nHere ...\n%s",raw);
+
+    return 0;
+
+}
